c592effddf86ba4e3807749a4461c0602cc5a150
Amaury Ricardo <ricardo901130@gmail.com>
2020-05-04 16:58:29 -0300

    Add main nav bar and bottom button

--- RSDemoProject/Home/Views/MainViewController.swift
+++ RSDemoProject/Home/Views/MainViewController.swift
@@ -11,21 +11,34 @@ import MapKit
 
 class MainViewController: UIViewController {
   
+  let mainTitleSpacing = 1.95
+  let createTargetLabelSpacing = 1.65
+  
   var viewModel: MainViewModel!
   @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var mainTItle: UILabel!
+  @IBOutlet weak var createTargetLabel: UILabel!
+  @IBOutlet weak var createNewTargetButton: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    viewModel = MainViewModel()
+    viewModel.delegate = self
     initMap()
+    initView()
   }
   
   private func initMap() {
     viewModel.requestCurrentLocation()
   }
   
+  private func initView() {
+    createTargetLabel.addSpacing(kernValue: createTargetLabelSpacing)
+    mainTItle.addSpacing(kernValue: mainTitleSpacing)
+  }
+  
   private func addCurrentLocation(_ location: CLLocation) {
+    mapView.center(location)
     mapView.addAnnotation(location: location)
   }
 }
